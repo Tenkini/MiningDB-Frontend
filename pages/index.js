9b302b82cb33246a65efbf329db058a9404c91b8
Tenkini <89607474+Tenkini@users.noreply.github.com>
2023-06-12 18:39:57 -0400

    Mi primer commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,141 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primeicons/primeicons.css';
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { ProductServiceRajo } from './tableData/rajoData';
+import { ProductServiceFase } from './tableData/faseData';
 
-const inter = Inter({ subsets: ['latin'] })
+export default function StripedRowsDemo() {
+  const [productsRajo, setProductsRajo] = useState([]);
+  const [productsFase, setProductsFase] = useState([]);
+  const [expandedRowsRajo, setExpandedRowsRajo] = useState([]);
+  const [expandedRowsFase, setExpandedRowsFase] = useState([]);
 
-export default function Home() {
+  const onRowToggleRajo = (event) => {
+    setExpandedRowsRajo(event.data);
+  };
+
+  const onRowToggleFase = (event) => {
+    setExpandedRowsFase(event.data);
+  };
+
+  useEffect(() => {
+    ProductServiceRajo.getProductsMini().then((data) => setProductsRajo(data));
+    ProductServiceFase.getProductsMini().then((data) => setProductsFase(data));
+  }, []);
+
+  const expandedRowTemplate = (rowData) => {
+    return (
+      <div className="expanded-row-content">
+        <DataTable
+          value={[rowData]} // Pass the expanded row data as an array
+          className="p-datatable-expanded-row hide-header" // Add the hide-header class
+          tableStyle={{ width: '100%' }}
+          header={null} // Set the header to null to hide it
+        >
+          <Column field="DiarioReal" />
+          <Column field="DiarioPlan" />
+          <Column field="KPI1" />
+          <Column field="SemanaISOReal" />
+          <Column field="SemanaISOPlan" />
+          <Column field="KPI2" />
+          <Column field="SemanaReal" />
+          <Column field="SemanaPlan" />
+          <Column field="KPI3" />
+          <Column field="MensualReal" />
+          <Column field="MensualPlan" />
+          <Column field="KPI4" />
+          <Column field="AnualReal" />
+          <Column field="AnualPlan" />
+          <Column field="KPI5" />
+        </DataTable>
+      </div>
+    );
+  };
+  
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="card">
+      <div className="banner flex justify-center">
+        <button className="banner-button hover:bg-blue-600 text-grey font-bold py-2 px-4 rounded mx-2">
+          Reportes
+        </button>
+        <button className="banner-button hover:bg-blue-600 text-grey font-bold py-2 px-4 rounded mx-2">
+          Editar carga
+        </button>
+        <button className="banner-button hover:bg-blue-600 text-grey font-bold py-2 px-4 rounded mx-2">
+          Crear usuario
+        </button>
+        <button className="banner-button hover:bg-blue-600 text-grey font-bold py-2 px-4 rounded mx-2">
+          Exportar
+        </button>
+        <button className="banner-button hover:bg-blue-600 text-grey font-bold py-2 px-4 rounded mx-2">
+          Salir
+        </button>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <DataTable
+        value={productsRajo}
+        expandedRows={expandedRowsRajo}
+        onRowToggle={onRowToggleRajo}
+        rowExpansionTemplate={expandedRowTemplate}
+        className="p-datatable-striped p-datatable-hoverable-rows"
+        tableStyle={{ minWidth: '50rem' }}
+      >
+        <Column
+          expander
+          style={{ width: '3rem', textAlign: 'center' }}
+          bodyStyle={{ textAlign: 'center' }}
+          headerStyle={{ textAlign: 'center' }}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <Column field="DiarioReal" header="Diario Real" />
+        <Column field="DiarioPlan" header="Diario Plan" />
+        <Column field="KPI1" header="KPI" />
+        <Column field="SemanaISOReal" header="Semana ISO Real" />
+        <Column field="SemanaISOPlan" header="Semana ISO Plan" />
+        <Column field="KPI2" header="KPI" />
+        <Column field="SemanaReal" header="Semana Real" />
+        <Column field="SemanaPlan" header="Semana Plan" />
+        <Column field="KPI3" header="KPI" />
+        <Column field="MensualReal" header="Mensual Real" />
+        <Column field="MensualPlan" header="Mensual Plan" />
+        <Column field="KPI4" header="KPI" />
+        <Column field="AnualReal" header="Anual Real" />
+        <Column field="AnualPlan" header="Anual Plan" />
+        <Column field="KPI5" header="KPI" />
+      </DataTable>
+      <DataTable
+        value={productsFase}
+        expandedRows={expandedRowsFase}
+        onRowToggle={onRowToggleFase}
+        rowExpansionTemplate={expandedRowTemplate}
+        className="p-datatable-striped p-datatable-hoverable-rows"
+        tableStyle={{ minWidth: '50rem' }}
+      >
+        <Column
+          expander
+          style={{ width: '3rem', textAlign: 'center' }}
+          bodyStyle={{ textAlign: 'center' }}
+          headerStyle={{ textAlign: 'center' }}
+        />
+        <Column field="DiarioReal" header="Diario Real" />
+        <Column field="DiarioPlan" header="Diario Plan" />
+        <Column field="KPI1" header="KPI" />
+        <Column field="SemanaISOReal" header="Semana ISO Real" />
+        <Column field="SemanaISOPlan" header="Semana ISO Plan" />
+        <Column field="KPI2" header="KPI" />
+        <Column field="SemanaReal" header="Semana Real" />
+        <Column field="SemanaPlan" header="Semana Plan" />
+        <Column field="KPI3" header="KPI" />
+        <Column field="MensualReal" header="Mensual Real" />
+        <Column field="MensualPlan" header="Mensual Plan" />
+        <Column field="KPI4" header="KPI" />
+        <Column field="AnualReal" header="Anual Real" />
+        <Column field="AnualPlan" header="Anual Plan" />
+        <Column field="KPI5" header="KPI" />
+      </DataTable>
+    </div>
+  );
 }
