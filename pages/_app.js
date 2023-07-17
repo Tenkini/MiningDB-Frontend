ab1bc41c209a4ca10f72be95925506ac0f1a9d7c
Whiserber <whiserber@gmail.com>
2023-07-16 21:45:21 -0400

    agregado transicion entre paginas

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
-import '@/styles/globals.css'
-import {ThemeProvider} from 'next-themes';
+import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import { motion } from "framer-motion";
 
-
-export default function App({ Component, pageProps }) {
-  return(
-    <ThemeProvider attribute='class' enableSystem={true}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+export default function App({ Component, pageProps, router }) {
+  return (
+    <motion.div
+      key={router.route}
+      initial="initial"
+      animate="animate"
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        animate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <ThemeProvider attribute="class" enableSystem={true}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </motion.div>
+  );
 }
